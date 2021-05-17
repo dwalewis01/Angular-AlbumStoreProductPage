4ccece393ac9234efee9aa572d7bf5d3e65e01e0
Dwayne Lewis <dwalewis@cisco.com>
2021-05-17 15:58:07 -0500

    Create service to get album data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-
+import { ProductService } from './product.service';
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,9 +17,9 @@ import { ProductDescriptionComponent } from './product-description/product-descr
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { pipe } from 'rxjs';
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+  constructor( private _http:HttpClient) { }
+
+  getAlbum(id:number):Observable<any> {
+
+  return this._http.get(this._albumUrl);
+  }
+
+}
