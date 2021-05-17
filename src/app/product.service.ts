81cad8607822042e136ee20fe700125e17ff618b
Dwayne Lewis <dwalewis@cisco.com>
2021-05-17 16:11:49 -0500

    correct error

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { pipe } from 'rxjs';
-import {Http , Response} from '@angular/http'
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  constructor( private _http:HttpClient) { }
-
-  getAlbum(id:number):Observable<any> {
-
-  return this._http.get(this._albumUrl);
+  constructor( private _http:Http) { }
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
