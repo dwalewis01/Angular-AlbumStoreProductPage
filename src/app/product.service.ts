a7b378a6aa5f54db039fbd940609270190553409
Dwayne Lewis <dwalewis@cisco.com>
2021-05-18 08:44:46 -0500

    Displaying a list of Products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,14 +4,20 @@ import { Album} from './album';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
+import { Product } from './product';
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productUrl = '../assets/products.json';
   //constructor( private _http:Http) { }
   constructor(private _http:HttpClient){}
   getAlbum(id: number):Observable<Album> {
     return this._http.get<Album>(this._albumUrl);
+  }
+  getProduct():Observable<Product[]>{
+
+    return this._http.get<Product[]>(this._productUrl);
   }
 
 }
